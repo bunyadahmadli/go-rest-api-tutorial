ea726d2004b626c8924d8bcab2872938dedfdfb2
Bunyad Ahmadli <34852029+bunyadahmadli@users.noreply.github.com>
2021-04-21 00:10:54 +0300

    json request

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+type User struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+}
+
 func mainHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Main endpointine get isteği yapıldı")
 }
 
-func userHandler(c echo.Context) error {
+func getUser(c echo.Context) error {
 	dataType := c.Param("data")
 	username := c.QueryParam("username")
 	name := c.QueryParam("name")
@@ -31,11 +39,27 @@ func userHandler(c echo.Context) error {
 	return c.String(http.StatusBadRequest, "Parametre olarak Json ve ya String gire bilirsiniz")
 }
 
+func addUser(c echo.Context) error {
+	user := User{}
+	body, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		return err
+	}
+	fmt.Println(user)
+	return c.String(http.StatusOK, "Başarılı")
+}
+
 func main() {
 	e := echo.New()
 
 	e.GET("/home", mainHandler)
-	e.GET("/user/:data", userHandler)
+	e.GET("/user/:data", getUser)
+	e.POST("/user", addUser)
 
 	e.Start(":8099")
 }
